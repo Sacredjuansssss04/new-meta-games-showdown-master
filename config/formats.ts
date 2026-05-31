797d37fee370d45221a87576b23cb17f02ebdf71
Sacredjuansssss04 <juanesteban04012007@gmail.com>
2026-05-30 21:15:31 -0500

    prueba 1

--- config/formats.ts
+++ config/formats.ts
@@ -30,7 +30,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		name: "[Gen 9] Bug OU",
 		mod: 'gen9',
 		ruleset: ['Standard NatDex', 'Force Monotype = Bug'],
-		banlist: [],
+		banlist: ['water'],
 	},
 
 	{
